92ec260a6b2832159cf997b283fd5905686300b1
Carl Hörberg <carl@84codes.com>
2025-08-01 17:35:24 +0200

    traceback not availabe in MP

--- main.py
+++ main.py
@@ -134,9 +134,3 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         print("\nShutting down...")
-    except Exception as e:
-        print(f"Error: {e}")
-
-        import traceback
-
-        traceback.print_exc()
